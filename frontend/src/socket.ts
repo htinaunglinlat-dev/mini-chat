1565309272e5154e34ed64b9ba76cb32ec0682a9
Htin Aung Linn Latt <htinaunglinlat1303@gmail.com>
2025-03-15 13:28:47 +0630

    fix2 in deployment

--- frontend/src/socket.ts
+++ frontend/src/socket.ts
@@ -2,7 +2,7 @@ import { io, Socket } from 'socket.io-client';
 
 // "undefined" means the URL will be computed from the `window.location` object
 // const URL = process.env.NODE_ENV === 'production' ? undefined : 'http://localhost:3000';
-const BASE_URL = 'http://localhost:3000';
+const BASE_URL = 'https://mini-chat-app-yvi3.onrender.com';
 
 let socket: Socket | null = null
 
